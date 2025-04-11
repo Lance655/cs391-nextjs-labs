a39e415fdc19b71e6a7ed341b12a77e6e00c44ab
Lance Sinson <ssinson@bu.edu>
2025-04-11 19:51:17 -0400

    apr11

---
+++ app/post/[id]/page.tsx
@@ -0,0 +1,20 @@
+import FullPost from "@/components/FullPost";
+import getPostById from "@/lib/getPostById";
+import { redirect } from "next/navigation"
+
+export default async function FullPostPage({ params } : { params: Promise<{id: string}>}) {
+    const { id } = await params;
+
+        try {
+            const post = await getPostById(id)
+            if (post === null) {
+                return redirect("/")
+            }
+            return <FullPost post={post}/>
+        } catch (err) {
+            console.error(err);
+            return redirect("/")
+        }
+
+    return redirect("/")
+}
